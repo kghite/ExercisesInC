456e86f073d0af3f80acba69635d2bb3735165fb
kghite <kghite@gmail.com>
2017-02-07 01:30:12 -0500

    Finish exercise 2

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -5,6 +5,20 @@ License: GNU GPLv3
 
 */
 
+// Question 1.  The file is supposed to create and array with 5 elements, then 
+// place 42 at each index.  The ouput will be the printed array.
+//
+// Question 2. Output is warning: function returns address of local variable.  This 
+// means that a function returns the address of a variable that is local to the 
+// fucntion.
+//
+// Question 3. I get the following line: 9146 segmentation fault (core dumped)  ./stack.
+//
+// Question 4.  The code prints the addresses of the arrays before throwing the 
+// segmentation fault when the lines are uncommented, and just throws the fault otherwise.
+// The two pointers are going to the same memory address, causing the main loop
+// to print values that are not allocated to the arrays after foo and bar have both run.
+
 #include <stdio.h>
 #include <stdlib.h>
 
